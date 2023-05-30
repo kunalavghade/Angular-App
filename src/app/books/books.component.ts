e87291ed5d7f656c962483b24755ce8e9ab94748
kunalavghade <kunalavghade77@example.com>
2023-05-30 18:08:33 +0530

    ngFor

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Book{
+  name: string
+  author: string
+  src : string
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-
 export class BooksComponent implements OnInit{
   
-  name1: string =  'Clean Code';
-  author1: string = 'Robert Cecil Martin';
-  img1: string =  "https://m.media-amazon.com/images/I/41zoxjP9lcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg";
+  books: Book[] = [
+    {
+      name: 'Clean Code',
+      author: 'Robert Cecil Martin',
+      src: "https://m.media-amazon.com/images/I/41zoxjP9lcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    },
+    {
+      name: 'Clean Code',
+      author: 'Robert Cecil Martin',
+      src: "https://m.media-amazon.com/images/I/41zoxjP9lcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    },
+    {
+      name: 'Clean Code',
+      author: 'Robert Cecil Martin',
+      src: "https://m.media-amazon.com/images/I/41zoxjP9lcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    }
+  ];
   
   BooksComponent() {}
 
@@ -21,9 +41,5 @@ export class BooksComponent implements OnInit{
   inputStr: string = '';
 
   isShowing: boolean = true;
-
-  // toggleBooks(){
-  //   this.isShowing = !this.isShowing;
-  // }
 
 }
