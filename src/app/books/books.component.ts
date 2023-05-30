14a1f422ea7bcdae93f48c9a93f3327f0f61a64c
kunalavghade <kunalavghade77@example.com>
2023-05-30 16:41:32 +0530

    Event Binding

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -12,12 +12,16 @@ export class BooksComponent implements OnInit{
   author1: string = 'Robert Cecil Martin';
   img1: string =  "https://m.media-amazon.com/images/I/41zoxjP9lcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg";
   
+  BooksComponent() {}
+
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
   isDisable : boolean = false;
 
-  BooksComponent() {}
-
+  handleClick(){
+    this.isDisable = true;
+    alert('working on click')
+  }
 }
