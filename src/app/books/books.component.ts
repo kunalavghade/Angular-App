c09946f799fc342c5344756f2761e7b533852be0
kunalavghade <kunalavghade77@example.com>
2023-06-02 16:57:14 +0530

    style

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -5,6 +5,7 @@ interface Book{
   name: string
   author: string
   src : string
+  ammount: number
 }
 
 @Component({
@@ -19,16 +20,19 @@ export class BooksComponent implements OnInit{
       name: 'Clean Code',
       author: 'Robert Cecil Martin',
       src: "https://m.media-amazon.com/images/I/41zoxjP9lcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+      ammount: 700,
     },
     {
       name: 'Clean Code',
       author: 'Robert Cecil Martin',
       src: "https://m.media-amazon.com/images/I/41zoxjP9lcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+      ammount: 700,
     },
     {
       name: 'Clean Code',
       author: 'Robert Cecil Martin',
       src: "https://m.media-amazon.com/images/I/41zoxjP9lcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+      ammount: 700,
     }
   ];
   
