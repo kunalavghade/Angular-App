1d38e378e9d53d11ed2e003d7910942dc411fde7
kunalavghade <kunalavghade77@example.com>
2023-06-03 17:21:03 +0530

    Login / singup tamplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { Appcomponent } from "./app.component";
+import { AuthModule } from "./auth/auth.module";
 import { BooksModule } from "./books/books.module";
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-    declarations : [Appcomponent, CartComponent],
-    imports : [BrowserModule, FormsModule, BooksModule, AppRoutingModule],
+    declarations : [Appcomponent, CartComponent ],
+    imports : [BrowserModule, FormsModule, BooksModule, AppRoutingModule, AuthModule],
     bootstrap : [Appcomponent]
 })
 export class AppModule {}
