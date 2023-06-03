458e3755850fb78d85b01c862ae77d22ae669690
kunalavghade <kunalavghade77@example.com>
2023-06-03 13:50:48 +0530

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
 import { Appcomponent } from "./app.component";
 import { BooksModule } from "./books/books.module";
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
     declarations : [Appcomponent, CartComponent],
-    imports : [BrowserModule, FormsModule, BooksModule],
+    imports : [BrowserModule, FormsModule, BooksModule, AppRoutingModule],
     bootstrap : [Appcomponent]
 })
 export class AppModule {}
